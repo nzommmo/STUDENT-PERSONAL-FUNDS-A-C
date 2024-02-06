7a89b10d34e27e3aae7e22756abe710ddd23bf71
nzommmo <ericnzomo17@gmail.com>
2024-02-06 18:24:02 +0300

    update

--- Registration/signup.js
+++ Registration/signup.js
@@ -6,8 +6,14 @@ const pass= password.value
 const repass= repassword.value 
 
 function signup(){
-    if((pass.value ==! repass.value)){
+    if(password.value !== repassword.value){
         alert("Passwords do not match")
         
     }
+    if (pass.length < 8){
+        alert("pasword is too short")
+
+    }
 }
+
+signupbtn.addEventListener("click",signup)
